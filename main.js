d951e010359bfd00fb6d6d2df11a218502b15291
kato83 <katomeganeb83@gmail.com>
2024-11-17 23:50:15 +0000

    confirmヒントは追加

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ const getTodos = () => {
   const storedTodos = localStorage.getItem('todos');
   return storedTodos ? JSON.parse(storedTodos) : [];
 };
+
+[...document.getElementsByClassName('delete-btn')]
+  .forEach(node => node.addEventListener('click', () => console.log(confirm('このTODOを削除してもよろしいですか？'))));
